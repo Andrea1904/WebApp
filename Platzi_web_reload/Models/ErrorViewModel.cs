aec551c5ddd064bf39edc767c45cbff3ae6702fc
AndreaLopez <andrea.lopez1904@gmail.com>
2021-05-08 22:51:06 -0500

    iniciando con web apps

---
+++ Platzi_web_reload/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Platzi_web_reload.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
